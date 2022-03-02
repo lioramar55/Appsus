2adcd56e6388ea2314a69c61a9ddac054f1156b8
Shunit Mizrachi <shuna1800@gmail.com>
2022-03-02 12:34:52 +0200

    added array of emails to mail service

--- js/apps/mail/services/mail-service.js
+++ js/apps/mail/services/mail-service.js
@@ -3,6 +3,37 @@
 import storageService from '../../../services/async-storage-service.js'
 import utilService from '../../../services/util-service.js'
 
+
+const emails = [
+   {
+    id: utilService.makeId(),
+    subject: 'Miss you!',
+    body: 'Would love to catch up sometimes',
+    isRead: false,
+    sentAt : 1551133930594,
+    to: 'momo@momo.com'
+    },
+    {
+      id: utilService.makeId(),
+      subject: 'Miss you!',
+      body: 'Would love to catch up sometimes',
+      isRead: false,
+      sentAt : 1551133930594,
+      to: 'momo@momo.com'
+      },
+        {
+        id: utilService.makeId(),
+        subject: 'Miss you!',
+        body: 'Would love to catch up sometimes',
+        isRead: false,
+        sentAt : 1551133930594,
+        to: 'momo@momo.com'
+        }
+    
+]
+
+
+
 export const mailService = {
   getMails,
 }
