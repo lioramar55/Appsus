0ffd447c3520aa77c9f33963e1662c6b721e441d
lioramar55 <lioramar55@gmail.com>
2022-03-02 13:26:25 +0200

    started styling the header

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,23 +1,25 @@
 export default {
   template: `
     <header class="app-header">
-      <div class="logo">LOGO</div>
-      <nav>
-        <ul>
-          <li>
-            <router-link to="/">Home</router-link>
-          </li>
-          <li>
-            <router-link to="/book">Books</router-link>
-          </li>
-          <li>
-            <router-link to="/keep">Keep</router-link>
-          </li>        
-          <li>
-            <router-link to="/mail">Mail</router-link>
-          </li>        
-        </ul>
-      </nav>
+      <section class="main-layout flex-space-between">
+        <div class="logo">LOGO</div>
+        <nav>
+          <ul class="nav-list clean-list">
+            <li>
+              <router-link to="/">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/book">Books</router-link>
+            </li>
+            <li>
+              <router-link to="/keep">Keep</router-link>
+            </li>        
+            <li>
+              <router-link to="/mail">Mail</router-link>
+            </li>        
+          </ul>
+        </nav>
+      </section>
     </header>
     `,
 }
