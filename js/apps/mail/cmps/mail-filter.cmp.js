020769884c03b43fb40cf6adec1e2305d4ec24d5
Shunit Mizrachi <shuna1800@gmail.com>
2022-03-05 07:31:07 +0200

    changed header style of the mail

--- js/apps/mail/cmps/mail-filter.cmp.js
+++ js/apps/mail/cmps/mail-filter.cmp.js
@@ -1,20 +1,22 @@
 export default {
   template: `
         <section class="mail-filter">
+        <div class="wrap">
           <label>
               <input v-on:keyup.enter="setFilter('txt')" type="text" name="text" v-model="filterBy.txt" 
               placeholder="Search for email" />
           </label>
-          <div class="wrap">
             <select value="All" @change="setFilter('isRead')" v-model="filterBy.isRead">
               <option >All</option>
               <option >Read</option>
               <option >Unread</option>
             </select>
+            </div>
+            <div class="wrap">
             <button @click="$emit('set-sort','title')">Sort by title</button>
-            <button @click="$emit('set-sort','date')">Sort by date</button>
-          </div>
-        </section>
+            <button @click="$emit('set-sort','date')">Sort by date</button> 
+            </div>    
+            </section>
     `,
   data() {
     return {
