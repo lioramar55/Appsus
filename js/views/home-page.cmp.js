f61735d474a354521a9a11ae273ca01d68f09fac
lioramar55 <lioramar55@gmail.com>
2022-03-05 21:30:56 +0200

    added a horse img to home page

--- js/views/home-page.cmp.js
+++ js/views/home-page.cmp.js
@@ -1,6 +1,7 @@
 export default {
   template: `
         <section class="home-page">
+          <img class="horse-img" src="assets/icons/horse.png">
           <h1>Welcome to Appsus! </h1>
           <h2>Your favorite apps in one place</h2>
           <p>A single page application with a set of mini apps for your convenience</p>
